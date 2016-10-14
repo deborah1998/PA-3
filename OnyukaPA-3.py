d4caac669ec43830fce8eb478a981ce69094806f
deborahonyuka <daonyuka@loyola.edu>
2016-10-14 09:02:24 -0400

    Deborah Onyuka-PA3

---
+++ OnyukaPA-3.py
@@ -0,0 +1,22 @@
+def inputfootball_numbers():
+    float(input("Please enter the number of completions:"))
+    float(input("Please enter the number of attempts:"))
+    float(input("Please enter the number of passing yards:"))
+    float(input("Please enter the number of touchdown passes"))
+    return inputfootball_numbers()
+def inputfencing_numbers():
+    float(input("Please enter the number of fencing participants:"))
+    float(input("Please enter the number of fencers ranked 1-8 in the World Cup Standings."))
+    float(input("Please enter the number of fencers ranked 9-16 in the World Cup Standings."))
+    float(input("Please enter the number of fencers ranked 17-32 in the World Cup Standings."))
+    float(input("Please enter the number of fencers ranked 33-64 in the World Cup Standings."))
+    float(input("Please enter the number of fencers ranked 65-100 in the World Cup Standings."))
+    float(input("Please enter the number of fencers ranked 1-16 in the Junior World Cup Standings."))
+    return inputfencing_numbers()
+def inputgymnastics_numbers():
+    float(input("Please enter the highest score for execution."))
+    float(input("Please enter the lowest score for execution."))
+    float(input("Please enter the third score for execution."))
+    float(input("Please enter the fourth score for execution."))
+    float (input("Please enter the fifth score for execution"))
+    float
